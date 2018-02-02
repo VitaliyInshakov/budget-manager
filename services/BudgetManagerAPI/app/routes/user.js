39e3a35a3bb92a0e8e4b340e3ef771eec220c7ea
Vitaliy Inshakov <michael8913@gmail.com>
2018-02-02 16:37:13 +0200

    add routes for user

---
+++ services/BudgetManagerAPI/app/routes/user.js
@@ -0,0 +1,10 @@
+const passport = require('passport'),
+      config = require('@config'),
+      models = require('@BudgetManager/app/setup');
+
+module.exports = (app) => {
+  const api = app.BudgetManagerAPI.app.api.user;
+  app.route('/api/v1/auth').post(api.setup(models.User));
+  app.route('/api/v1/users').get(passport.authenticate('jwt', config.session), api.index(models.User, app.get('budgetsecret')));
+  app.route('/api/v1/signup').post(api.signup(models.User));
+}
