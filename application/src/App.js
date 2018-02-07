72ada31b4cd5f27e3a7f01d48f7fb1cea9d70e2c
Vitaliy Inshakov <michael8913@gmail.com>
2018-02-07 13:18:08 +0200

    add redux, styles, image

--- application/src/App.js
+++ application/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Authentication from './components/pages/Authentication/Authentication';
 
+import './assets/style.scss';
+
 class App extends Component {
   render() {
     return (
