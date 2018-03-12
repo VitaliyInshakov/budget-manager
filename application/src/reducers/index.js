58af2b270b49288747e7c8a0e8e10fba20c3e679
Vitalii Inshakov <michael8913@gmail.com>
2018-03-13 00:33:34 +0200

    add one new reducer

--- application/src/reducers/index.js
+++ application/src/reducers/index.js
@@ -3,12 +3,14 @@ import { routerReducer } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 import authReducer from './authReducer';
 import budgetReducer from './budgetReducer';
+import homeReducer from './homeReducer';
 
 const reducer = combineReducers({
   form,
   router: routerReducer,
   auth: authReducer,
-  budget: budgetReducer
+  budget: budgetReducer,
+  home: homeReducer
 })
 
 export default reducer;
