b9d1cef6d98c08231ee6d6a15260a3719df9be6f
Vitalii Inshakov <michael8913@gmail.com>
2018-03-14 23:34:16 +0200

    add methods

--- application/src/components/List/ListBody.js
+++ application/src/components/List/ListBody.js
@@ -1,31 +1,81 @@
 import React from 'react';
 
-const BudgetListBody = (props) => (
-  <section className="list-body">
-    {
-      props.data
-      ?
-        props.data.map(item => {
-          return (
-            <div className="md-list-item">
-              {item.info.map(info => {
-                return (
-                  (info !== item._id) 
-                  ? <div classname={props.budgetsVisible ? "md-budget-info text-white" : "md-client-info text-white"}>{info}</div>
-                  : null
-                )                
-              })}
-              <div className={props.budgetsVisible ? "budget-actions" : "client-actions"}>
-                <i className="material-icons btn btn--flat btn--raised btn--small light-blue--text text--lighten-1">visibility</i>
-                <i className="material-icons btn btn--flat btn--raised btn--small yellow--text text--accent-1">mode_edit</i>
-                <i className="btn btn--flat btn--raised btn--small red--text text--lighten-1">delete_forever</i>
+const BudgetListBody = (props) => {
+  function getItemAndEdit(item) {
+    const authHeader = `Bearer ${localStorage.getItem('token')}`
+    !item.phone ? props.getBudget(authHeader, item) : props.getClient(authHeader, item);
+  }
+  function handleClickDeleteItem(item) {
+    props.deleteItem(item, props.data, props.budgetsVisible, `Bearer ${localStorage.getItem('token')}`);
+  }
+   return (
+    <section className="list-body">
+      {
+        (props.data != null && props.parsedBudgets === null)
+        ?
+          props.data.map((item, idx) => {
+            return (
+              <div key={idx} className="md-list-item">
+                {Object.keys(item).map(info => {
+                  return (
+                    (item[info] !== item._id && item[info] !== item.client_id) 
+                    ? <div key={info} className={props.budgetsVisible ? "md-budget-info text-white" : "md-client-info text-white"}>{item[info]}</div>
+                    : null
+                  )                
+                })}
+                <div className={props.budgetsVisible ? "budget-actions" : "client-actions"}>
+                  <i
+                    className="material-icons icon btn btn--flat btn--raised btn--small yellow--text text--accent-1"
+                    onClick={getItemAndEdit.bind(null, item)}
+                  >
+                    mode_edit
+                  </i>
+                  <i
+                    className="material-icons icon btn btn--flat btn--raised btn--small red--text text--lighten-1"
+                    onClick={handleClickDeleteItem.bind(null,item)}
+                  >
+                    delete_forever
+                  </i>
+                </div>
               </div>
-            </div>
-          )
-        })
-      : null
-    }
-  </section>
-)
+            )
+          })
+        : null
+      }
+      {
+        props.parsedBudgets !== null
+        ?
+          props.parsedBudgets.map((item, idx) => {
+            return (
+              <div key={idx} className="md-list-item">
+                {Object.keys(item).map(info => {
+                  return (
+                    (item[info] !== item._id && item[info] !== item.client_id) 
+                    ? <div key={info} className={props.budgetsVisible ? "md-budget-info text-white" : "md-client-info text-white"}>{item[info]}</div>
+                    : null
+                  )                
+                })}
+                <div className={props.budgetsVisible ? "budget-actions" : "client-actions"}>
+                  <i
+                    className="material-icons icon btn btn--flat btn--raised btn--small yellow--text text--accent-1"
+                    onClick={getItemAndEdit.bind(null, item)}
+                  >
+                    mode_edit
+                  </i>
+                  <i
+                    className="material-icons icon btn btn--flat btn--raised btn--small red--text text--lighten-1"
+                    onClick={handleClickDeleteItem.bind(null,item)}
+                  >
+                    delete_forever
+                  </i>
+                </div>
+              </div>
+            )
+          })
+        : null
+      }
+    </section>
+   )
+}
 
 export default BudgetListBody;
